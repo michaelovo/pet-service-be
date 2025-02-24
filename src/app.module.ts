802fe9dd231e75d3dcf9f3acf68faf4c47ff4769
michaelovo <emikeovo@gmail.com>
2025-02-24 21:41:29 +0100

    Refactor to use env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,35 @@ import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { Cat } from './cats/entities/cat.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'itscope@2024',
-      database: 'nextjs_typeorm_quiz_db',
-      entities: [Cat],
-      synchronize: true,
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: 'localhost',
+    //   port: 3306,
+    //   username: 'root',
+    //   password: 'itscope@2024',
+    //   database: 'nextjs_typeorm_quiz_db',
+    //   entities: [Cat],
+    //   synchronize: true,
+    // }),
+    // CatsModule,
+    ConfigModule.forRoot(), // Load .env file
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [Cat],
+        synchronize: true,
+      }),
     }),
     CatsModule,
   ],
